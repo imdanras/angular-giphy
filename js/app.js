e3d9455ad8cb11182d0a3b14c9d614d83c76bd8f
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-07-23 20:30:54 -0700

    search results show up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
-angular.module('GiphyApp', [])
+  angular.module('GiphyApp', [])
 
-.run(function() {
-  console.log('App has loaded!');
-});
+.controller('GiphyCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.search = '';
+  $scope.searchGiphy = searchGiphy
+
+  $scope.path = function() {
+    for (var i = 0; i < data.length; i++) {
+      return data[i].length.fixed_height.url;
+    }
+}
+
+
+function searchGiphy() {
+  console.log('success!')
+  $http.get('http://api.giphy.com/v1/gifs/search?q=' +
+  $scope.search + '&api_key=dc6zaTOxFJmzC')
+  .then(function success(res) {
+    $scope.giphy = res.data.data;
+    $scope.search = '';
+    // console.log(res);
+  }, function error(res) {
+    console.log(res);
+  });
+}
+$scope.watch = ('search', function(){
+  searchGiphy();
+})
+}]);
