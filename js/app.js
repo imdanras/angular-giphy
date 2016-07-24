ebcbfc838153c48727f9e13e382bcaaafaa22e29
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-07-24 10:23:52 -0700

    working again. working on infinite scroll

--- js/app.js
+++ js/app.js
@@ -1,22 +1,40 @@
-  angular.module('GiphyApp', [])
+angular.module('GiphyApp', ['infinite-scroll'])
 
 .controller('GiphyCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.search = '';
-  $scope.searchGiphy = searchGiphy
+  $scope.searchGiphy = searchGiphy;
 
 function searchGiphy() {
   console.log('success!')
-  $http.get('http://api.giphy.com/v1/gifs/search?q=' +
-  $scope.search + '&api_key=dc6zaTOxFJmzC')
+  $http.get('http://api.giphy.com/v1/gifs/search', {
+    params: {
+      q: $scope.search,
+      api_key: 'dc6zaTOxFJmzC'
+    }
+  })
   .then(function success(res) {
+    // $scope.search = '';
     $scope.giphy = res.data.data;
-    $scope.search = '';
-    // console.log(res);
+    console.log($scope.giphy);
   }, function error(res) {
     console.log(res);
   });
 }
-$scope.watch = ('search', function(){
-  searchGiphy();
-})
+
+  // $scope.$watch = ('search', function(newValue, oldValue) {
+  //   $http.get('http://api.giphy.com/v1/gifs/search', {
+  //     params: {
+  //       q: $scope.search,
+  //       api_key: 'dc6zaTOxFJmzC'
+  //     }
+  //   }).then(function success(res) {
+  //     $scope.giphy = res.data.data;
+  //   }, function error(res) {
+  //     console.log(res);
+  //   })
+  // });
+
+
+
+
 }]);
