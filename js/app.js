57bb920279dfd83ed6a4996a7accc8224deed3ea
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-08-06 17:58:24 -0700

    added background gradient. working on results always clearing after new search and infinite scroll or pagination

--- js/app.js
+++ js/app.js
@@ -4,22 +4,23 @@ angular.module('GiphyApp', ['infinite-scroll'])
   $scope.search = '';
   $scope.searchGiphy = searchGiphy;
 
-function searchGiphy() {
-  console.log('success!')
-  $http.get('http://api.giphy.com/v1/gifs/search', {
-    params: {
-      q: $scope.search,
-      api_key: 'dc6zaTOxFJmzC'
-    }
-  })
+  function searchGiphy() {
+    console.log('success!');
+    $http.get('http://api.giphy.com/v1/gifs/search', {
+      params: {
+        q: $scope.search,
+        api_key: 'dc6zaTOxFJmzC'
+      }
+    })
   .then(function success(res) {
-    // $scope.search = '';
+    $scope.giphy = [];
+    $scope.search = '';
     $scope.giphy = res.data.data;
-    console.log($scope.giphy);
+    // console.log($scope.giphy);
   }, function error(res) {
     console.log(res);
   });
-}
+  }
 
   // $scope.$watch = ('search', function(newValue, oldValue) {
   //   $http.get('http://api.giphy.com/v1/gifs/search', {
@@ -33,8 +34,4 @@ function searchGiphy() {
   //     console.log(res);
   //   })
   // });
-
-
-
-
 }]);
